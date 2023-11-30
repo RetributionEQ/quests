d9d0b4823bf79c473346b477efe8f70e7ea492af
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2023-11-30 13:57:43 -0600

    test

--- global/global_player.pl
+++ global/global_player.pl
@@ -14,12 +14,7 @@ sub EVENT_ENTERZONE {
 }
 
 sub EVENT_ZONE {
-	quest::debug("from_zone_id " . $from_zone_id);
-	quest::debug("from_instance_id " . $from_instance_id);
-	quest::debug("from_instance_version " . $from_instance_version);
-	quest::debug("target_zone_id " . $target_zone_id);
-	quest::debug("target_instance_id " . $target_instance_id);
-	quest::debug("target_instance_version " . $target_instance_version);
+	return 1;
 }
 
 sub EVENT_CONNECT {
