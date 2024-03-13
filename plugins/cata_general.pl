9823c850ddf438ca9a65495c5bec6d56d8da72e3
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-13 10:40:56 -0500

    f

--- plugins/cata_general.pl
+++ plugins/cata_general.pl
@@ -43,21 +43,36 @@ sub CheckCashPayment {
     my $remaining_value = $total_value - $target_value;
 
     if ($remaining_value >= 0) {
-        # Calculate the change to give back as well as the actual amounts spent
-        my $change_platinum = int($remaining_value / 1000);
-        $remaining_value %= 1000;
+        # Calculate how much of each currency is needed to meet the target value
+        # Start by converting everything to copper for comparison
+        my $total_copper = $initial{copper} + ($initial{silver} * 10) + ($initial{gold} * 100) + ($initial{platinum} * 1000);
+        
+        # Determine how much of each currency is spent to meet the target
+        my $spent_copper = $target_value % 10;
+        my $spent_silver = int(($target_value % 100) / 10);
+        my $spent_gold = int(($target_value % 1000) / 100);
+        my $spent_platinum = int($target_value / 1000);
 
-        my $change_gold = int($remaining_value / 100);
-        $remaining_value %= 100;
+        # Update the total target value to include full coins only
+        my $adjusted_target = ($spent_platinum * 1000) + ($spent_gold * 100) + ($spent_silver * 10) + $spent_copper;
 
-        my $change_silver = int($remaining_value / 10);
-        my $change_copper = $remaining_value % 10;
+        # Calculate remaining values after spending towards the target
+        my $remaining_copper_after_target = $total_copper - $adjusted_target;
 
+        # Calculate actual remaining currency by converting remaining copper back to denominations
+        my $actual_remaining_platinum = int($remaining_copper_after_target / 1000);
+        $remaining_copper_after_target %= 1000;
+        my $actual_remaining_gold = int($remaining_copper_after_target / 100);
+        $remaining_copper_after_target %= 100;
+        my $actual_remaining_silver = int($remaining_copper_after_target / 10);
+        my $actual_remaining_copper = $remaining_copper_after_target % 10;
+
+        # Determine actual spent amounts in each currency
         my %actual_spent = (
-            platinum => $initial{platinum} - $change_platinum,
-            gold     => $initial{gold} - $change_gold,
-            silver   => $initial{silver} - $change_silver,
-            copper   => $initial{copper} - $change_copper,
+            platinum => $initial{platinum} - $actual_remaining_platinum,
+            gold     => $initial{gold} - $actual_remaining_gold,
+            silver   => $initial{silver} - $actual_remaining_silver,
+            copper   => $initial{copper} - $actual_remaining_copper,
         );
 
         my $message = "You give ";
@@ -68,11 +83,11 @@ sub CheckCashPayment {
         $message .= join(' ', @messages) . " to " . $npc->GetCleanName() . ".";
         $client->Message(276, $message);
 
-        # Inform the client of the change they receive, if any
-        if ($change_platinum > 0 || $change_gold > 0 || $change_silver > 0 || $change_copper > 0) {
+        # Construct and send the change message if there is remaining value
+        if ($remaining_copper_after_target > 0) {
             my $change_message = "You receive ";
             my @change_parts;
-            foreach my $currency (['platinum', $change_platinum], ['gold', $change_gold], ['silver', $change_silver], ['copper', $change_copper]) {
+            foreach my $currency (['platinum', $actual_remaining_platinum], ['gold', $actual_remaining_gold], ['silver', $actual_remaining_silver], ['copper', $actual_remaining_copper]) {
                 push @change_parts, "$currency->[1] $currency->[0]" if ($currency->[1] > 0);
             }
             $change_message .= join(' ', @change_parts) . " in change from " . $npc->GetCleanName() . ".";
@@ -80,8 +95,8 @@ sub CheckCashPayment {
         }
 
         # Provide the change back to the player's account
-        $client->AddMoneyToPP($change_copper, $change_silver, $change_gold, $change_platinum, 1);
-        
+        $client->AddMoneyToPP($actual_remaining_copper, $actual_remaining_silver, $actual_remaining_gold, $actual_remaining_platinum, 1);
+
         return 1;
     } else {
         # Refund all the money since the target value was not met
