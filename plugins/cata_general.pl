8e58107ac37f935b90e119c27857b82a0975df7a
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-13 09:58:13 -0500

    finalize CheckCashPayment

--- plugins/cata_general.pl
+++ plugins/cata_general.pl
@@ -24,10 +24,39 @@ sub ClassType {
 
 sub CheckCashPayment {
     my $client = plugin::val('$client');
-    my ($target_value, $copper, $silver, $gold, $platinum) = @_;
-    my $remaining_value = ($platinum * 1000) + ($gold * 100) + ($silver * 10) + $copper - $target_value;
+    my $npc    = plugin::val('$npc');
+    my ($target_value, $initial_copper, $initial_silver, $initial_gold, $initial_platinum) = @_;
+    my $total_value = $initial_platinum * 1000 + $initial_gold * 100 + $initial_silver * 10 + $initial_copper;
+    my $remaining_value = $total_value - $target_value;
 
     if ($remaining_value >= 0) {
-        $client->TakeMoneyFromPP($target_value, 1);
+        my ($return_platinum, $return_gold, $return_silver, $return_copper) = (
+            int($remaining_value / 1000),
+            int(($remaining_value % 1000) / 100),
+            int(($remaining_value % 100) / 10),
+            $remaining_value % 10
+        );
+
+        my %spent = (
+            platinum => $initial_platinum   - $return_platinum,
+            gold     => $initial_gold       - $return_gold,
+            silver   => $initial_silver     - $return_silver,
+            copper   => $initial_copper     - $return_copper,
+        );
+
+        my $message = "You give ";
+        my @messages;
+        foreach my $currency (qw(platinum gold silver copper)) {
+            push @messages, "$spent{$currency} $currency" if $spent{$currency} > 0;
+        }
+
+        $message .= join(' ', @messages) . " to " . $npc->GetCleanName() . ".";
+
+        $client->AddMoneyToPP($return_copper, $return_silver, $return_gold, $return_platinum, 1);
+        $client->Message(276, $message);
+        return 1;
+    } else {
+        $client->Message(276, "You do not have enough money. Please try again.");
+        return 0;
     }
 }
