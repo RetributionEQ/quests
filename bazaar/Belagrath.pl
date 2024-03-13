5282abe4b87555578272d2f080032502e90e55a0
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-12 21:01:59 -0500

    test

--- bazaar/Belagrath.pl
+++ bazaar/Belagrath.pl
@@ -15,47 +15,6 @@ sub EVENT_SAY {
 }
 
 sub EVENT_ITEM {
-  use Scalar::Util qw(looks_like_number);
-
-	my %item_data = (
-		0 => [ plugin::val('$item1'), plugin::val('$item1_charges'), plugin::val('$item1_attuned'), plugin::val('$item1_inst') ],
-		1 => [ plugin::val('$item2'), plugin::val('$item2_charges'), plugin::val('$item2_attuned'), plugin::val('$item2_inst') ],
-		2 => [ plugin::val('$item3'), plugin::val('$item3_charges'), plugin::val('$item3_attuned'), plugin::val('$item3_inst') ],
-		3 => [ plugin::val('$item4'), plugin::val('$item4_charges'), plugin::val('$item4_attuned'), plugin::val('$item4_inst') ],
-	);
-
-	foreach my $k (keys %itemcount) {
-		next if ($k eq "copper" || $k eq "silver" || $k eq "gold" || $k eq "platinum" || $k == 0);
-		my $rcount = $itemcount->{$k};
-		my $r;
-    quest::debug($k);
-    $k = $k % 1000000;
-    quest::debug($k);
-		for ($r = 0; $r < 4; $r++) {
-			if ($rcount > 0 && $item_data{$r}[0] && $item_data{$r}[0] == $k) {
-				if ($client) {
-					my $inst = $item_data{$r}[3];
-					my $return_count = $inst->RemoveTaskDeliveredItems();
-					if ($return_count > 0) {						
-						$client->SummonFixedItem($k, $inst->GetCharges(), $item_data{$r}[2]);
-						$return_data{$r} = [$k, $item_data{$r}[1], $item_data{$r}[2]];
-						$items_returned = 1;
-						next;
-					}
-					$return_data{$r} = [$k, $item_data{$r}[1], $item_data{$r}[2]];					
-					$client->SummonFixedItem($k, $item_data{$r}[1], $item_data{$r}[2]);
-					$items_returned = 1;
-				} else {
-					$return_data{$r} = [$k, $item_data{$r}[1], $item_data{$r}[2]];					
-					quest::summonfixeditem($k, 0);
-					$items_returned = 1;
-				}
-				$rcount--;
-			}
-		}
-
-		delete $itemcount->{$k};
-	}
-
-  quest::say("I have disenchanted these items for you...");
+  quest::say("I am not currently able to disenchant items");
+  plugin::return_items(\%itemcount);
 }
