de470f94ae28c8a22db182154d7191c1929b8ea7
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-03-12 20:05:36 -0500

    planning for disenchanter

--- bazaar/Belagrath.pl
+++ bazaar/Belagrath.pl
@@ -10,10 +10,18 @@ sub EVENT_TIMER {
 sub EVENT_SAY {
   if ($text=~/hail/i) {
     quest::say("Hello $name! I can disenchant a piece of Apocryphal or Rose Colored for you, returning it to it's original state. Please, only hand one item in at a time! And no, I will not undo the magic once it is done!");
+
   }  
 }
 
 sub EVENT_ITEM {
+  use Scalar::Util qw(looks_like_number);
+  foreach my $item in (keys %itemcount) {
+    quest::debug("$item");
+    if ($item != 'copper' && $item != 'silver' && $item != 'gold' && $item != 'platinum') {
+      
+    }
+  }
 	plugin::return_items(\%itemcount);
 }
 
