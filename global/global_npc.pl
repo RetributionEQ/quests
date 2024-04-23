442372e496797bb71a6ccc86de3964d187c7966a
catapultam-habeo <97849758+catapultam-habeo@users.noreply.github.com>
2024-04-23 09:00:01 -0500

    reduce perl spam

--- global/global_npc.pl
+++ global/global_npc.pl
@@ -85,37 +85,39 @@ sub EVENT_SAY {
 }
 
 sub EVENT_DEATH_COMPLETE {
-    my $corpse = $entity_list->GetCorpseByID($killed_corpse_id);
-    
-    my %item_drops = (
-        976011 => { #Fading Green Memory 976011
-            'drop_chance' => 0.001, # 1/1000% chance to drop
-            'min_level'   => 1, # Minimum level to drop from
-            'max_level'   => 99, # Maximum level to drop from
-        },
+    if (defined($killed_corpse_id)) {
+        my $corpse = $entity_list->GetCorpseByID($killed_corpse_id);
+        
+        my %item_drops = (
+            976011 => { #Fading Green Memory 976011
+                'drop_chance' => 0.001, # 1/1000% chance to drop
+                'min_level'   => 1, # Minimum level to drop from
+                'max_level'   => 99, # Maximum level to drop from
+            },
 
-        11703 => { #Box of Abu Kar 11703
-            'drop_chance' => 0.0001, # 1/1000% chance to drop
-            'min_level'   => 35, # Minimum level to drop from
-            'max_level'   => 99, # Maximum level to drop from
-        }
-        # ... more items and their attributes
-    );
+            11703 => { #Box of Abu Kar 11703
+                'drop_chance' => 0.0001, # 1/1000% chance to drop
+                'min_level'   => 35, # Minimum level to drop from
+                'max_level'   => 99, # Maximum level to drop from
+            }
+            # ... more items and their attributes
+        );
 
-    for my $item_id (keys %item_drops) {
-        if ($npc->GetLevel() >= $item_drops{$item_id}{'min_level'} && 
-            $npc->GetLevel() <= $item_drops{$item_id}{'max_level'}) {                    
-            if (rand() < $item_drops{$item_id}{'drop_chance'}) {
-                $corpse->AddItem($item_id); # Add the item to the NPC's inventory
-                quest::ding(); # Play the 'ding' sound, indicating an item drop or another significant event
+        for my $item_id (keys %item_drops) {
+            if ($npc->GetLevel() >= $item_drops{$item_id}{'min_level'} && 
+                $npc->GetLevel() <= $item_drops{$item_id}{'max_level'}) {                    
+                if (rand() < $item_drops{$item_id}{'drop_chance'}) {
+                    $corpse->AddItem($item_id); # Add the item to the NPC's inventory
+                    quest::ding(); # Play the 'ding' sound, indicating an item drop or another significant event
+                }
             }
         }
-    }
-    
-    if (quest::get_data("eom_17779")) {
-        my @lootlist = $corpse->GetLootList();
-        foreach my $item (@lootlist) {
-            quest::debug($item);           
+        
+        if (quest::get_data("eom_17779")) {
+            my @lootlist = $corpse->GetLootList();
+            foreach my $item (@lootlist) {
+                quest::debug($item);           
+            }
         }
     }
 }
